66dd8014051f36bbc994d11ab17f21dd996ce1c4
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-11-01 16:51:38 +0600

    generate link for view from html, load static files

--- chapter-one/pages/urls.py
+++ chapter-one/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import homePageView, aboutPageView, contactPageView
+from .views import homePageView, aboutPageView, contactPageView, profilePageView
 
 
 urlpatterns = [
     path("", homePageView, name="home"),
     path("about", aboutPageView, name="about"),
-    path("contact", contactPageView, name="contact")
+    path("contact", contactPageView, name="contact"),
+    path("profile/<int:id>", profilePageView, name="profile")
 ]
--- chapter-one/pages/views.py
+++ chapter-one/pages/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 def homePageView(request):
-    print("home page request.")
+
+    context = {
+        "name": "Munna",
+        "dept": "cse"
+    }
+
     
-    return HttpResponse("Hello World")
+    return TemplateResponse(request, "pages/home.html", context)
 
 
 def aboutPageView(request):
@@ -16,3 +22,7 @@ def contactPageView(request):
     context = {"name": "munna", "address": "Dhaka", "phone": "01773100924"}
 
     return JsonResponse(context)
+
+
+def profilePageView(request, id):
+    return JsonResponse({"name": "ifti", "id": id})
