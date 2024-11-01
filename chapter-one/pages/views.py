06ea1b4dd05b4173eb38962c6e8e020e42bf702b
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-11-01 15:00:43 +0600

    different response from view

--- chapter-one/pages/views.py
+++ chapter-one/pages/views.py
@@ -1,7 +1,18 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def homePageView(request):
-    print("home page request :", request)
+    print("home page request.")
     
     return HttpResponse("Hello World")
+
+
+def aboutPageView(request):
+
+    return render(request, "pages/about.html")
+
+
+def contactPageView(request):
+    context = {"name": "munna", "address": "Dhaka", "phone": "01773100924"}
+
+    return JsonResponse(context)
