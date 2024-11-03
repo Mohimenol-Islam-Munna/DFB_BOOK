2f8bfd2bbb181d5b978d92be5135ce9c3c4cc6ff
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-11-03 20:48:53 +0600

    ok

--- chapter-one/pages/urls.py
+++ chapter-one/pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import homePageView
-
+from .views import homePageView, AboutPageView
 
 urlpatterns = [
-    path("", homePageView, name="home")
+    path("", homePageView, name="home"),
+    path("about/", AboutPageView.as_view(), name="about/"),
 ]
--- chapter-one/pages/views.py
+++ chapter-one/pages/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def homePageView(request):
     print("home page request :", request)
     
     return HttpResponse("Hello World")
+
+class AboutPageView(TemplateView):
+    print("----------------about page request :")
+
+    template_name = "about.html"
