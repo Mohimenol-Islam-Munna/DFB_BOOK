88a4e79709152a54636029cdde34081db85f8dc7
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-11-04 12:07:16 +0600

    api call using jquery ajax

--- chapter-one/pages/static/pages/js/base.js
+++ chapter-one/pages/static/pages/js/base.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-  console.log("This comes from jquery");
+  // console.log("This comes from jquery");
 
   const title = $("#title");
   title.text("This title comes by jquery selector");
   title.text(`${title.text()}. ${"This title comes by jquery selector 2"}`);
 
   const content = $("#content");
-  console.log("content html::", content.html());
+  // console.log("content html::", content.html());
 
-  console.log("attr :", content.attr("id"));
+  // console.log("attr :", content.attr("id"));
 
   content.append("<div id='new' class='red'>This is appended by jQuery</div>");
 
--- chapter-one/pages/static/pages/js/home.js
+++ chapter-one/pages/static/pages/js/home.js
@@ -1,1 +1,37 @@
 console.log("this js file linked with home.html");
+
+let loading = false;
+
+$(document).ready(function () {
+  loading = true;
+
+  console.log("home document loaded 1:", loading);
+
+  const fetchData = async () => {
+    await $.ajax({
+      url: "https://jsonplaceholder.typicode.com/users",
+      type: "GET",
+      dataType: "json",
+      success: function (res, status, xhr) {
+        console.log("success res :", res);
+        console.log("success status :", status);
+        console.log("success xhr :", xhr);
+
+        loading = false;
+      },
+      error: function (xhr, status, error) {
+        console.log("success error :", error);
+        console.log("success status :", status);
+        console.log("success xhr :", xhr);
+
+        loading = false;
+      },
+    });
+
+    console.log("home document loaded 1 in:", loading);
+  };
+
+  fetchData();
+
+  console.log("home document loaded 2:", loading);
+});
