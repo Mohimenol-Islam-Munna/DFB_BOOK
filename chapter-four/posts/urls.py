557ba8ca6b9094f0e687c70b7e0d94e969ea15c9
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-11-06 15:38:16 +0600

    template generic list view

--- chapter-four/posts/urls.py
+++ chapter-four/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homeView
+from .views import homeView, PostPageView
 
 urlpatterns = [
-    path("", homeView, name="home")
+    path("", homeView, name="home"),
+    path("posts", PostPageView.as_view(), name="posts")
 ]
--- chapter-four/posts/views.py
+++ chapter-four/posts/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Post
 
 def homeView(request):
     return render(request, "posts/home.html", {"title": "you are right"})
+
+
+class PostPageView(ListView):
+    model = Post
+    template_name = "posts/post.html"
+    context_object_name = "post_list"
+
