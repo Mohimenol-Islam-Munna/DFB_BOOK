33c60d2f7448a06bf627fc4b4f6b4bb03360e4ae
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-11-11 19:21:44 +0600

    create view

--- chapter-four/blog/urls.py
+++ chapter-four/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogPageView, BlogDetailPageView
+from .views import BlogPageView, BlogDetailPageView, BlogCreateView
 
 urlpatterns = [
+    path("create/", BlogCreateView.as_view(), name="blog_create"),
     path("<int:pk>", BlogDetailPageView.as_view(), name="blog_detail"),
     path("", BlogPageView.as_view(), name="blog_home"),
 ]
--- chapter-four/blog/views.py
+++ chapter-four/blog/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+
 from .models import Post
 
 class BlogPageView(ListView):
@@ -11,3 +13,9 @@ class BlogDetailPageView(DetailView):
     model = Post
     template_name = "blog/detail.html"
     context_object_name = "post"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name= "blog/create.html"
+    fields = ["title", "author", "body"]
